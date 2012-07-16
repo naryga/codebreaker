072d78acf2de56844c7a4e5ae87615639d30b98a
Nathan Garza <nathan.garza@pinnacol.com>
2012-07-16 16:00:22 -0600

    finished the inishal rspec spec doc, about to refactor

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -1,7 +1,11 @@
 module CodeBreaker
   class Game
+    def initialize(output)
+      @output = output
+    end
     def start
-      
+      @output.puts 'Welcome to Codebreaker!'
+      @output.puts 'Enter guess:'
     end
   end
 end
