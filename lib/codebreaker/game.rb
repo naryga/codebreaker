a32fb965512dd98ea6baaf88ef118b2f2331763b
Nathan Garza <nathan.garza@pinnacol.com>
2012-07-17 09:23:36 -0600

    add tests and code for begining of code-breaker submits guess scenario

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -3,9 +3,19 @@ module CodeBreaker
     def initialize(output)
       @output = output
     end
-    def start
+    
+    def start(secret)
+      @secret = secret
       @output.puts 'Welcome to Codebreaker!'
       @output.puts 'Enter guess:'
+    end
+    
+    def guess(guess)
+      if @secret.include?(guess[0])
+        @output.puts '-'
+      else
+        @output.puts ''
+      end
     end
   end
 end
